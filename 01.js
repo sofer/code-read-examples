480b68fbe3e98b13a0395019539b52ffffc295f6
D Sofer <d@sofer.com>
2023-03-11 18:27:14 +0000

    Introducing an error to 01.js

--- 01.js
+++ 01.js
@@ -1,6 +1,6 @@
 function transform(arg1) {
     let result = [];
-    for (let i = 0; i < arg1.length; i++) {
+    for (let i = 0; i < arg1.length; i++ {
         result.push(arg1[i] + 1)
     }
     return result
